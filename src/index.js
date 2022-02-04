b4a595e82ccb5cb2a975eb988a0c48f7dbd83901
iamolegk1 <oleg.kropivka5@gmail.com>
2022-02-04 14:43:41 +0200

    indexDB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
 import Routers from "./Routers";
+
+import "./index.css";
 
 ReactDOM.render(<Routers />, document.getElementById("root"));
--- src/pages/Boards/index.jsx
+++ src/pages/Boards/index.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useMemo } from "react";
+
+import GridWrapper from "../../components/GridWrapper";
+import { WIDTHS } from "../../constants/layout";
+
+import BoardCard from "./components/BoardCard";
 
 const Boards = () => {
-  return <div>Boards text</div>;
+  const boardsList = useMemo(
+    () => [
+      {
+        id: 0,
+        Component: () => <BoardCard title="Lorem ipsum dolor sit amet" />,
+      },
+      {
+        id: 1,
+        Component: () => <BoardCard title="Lorem ipsum dolor sit amet" />,
+      },
+      {
+        id: 2,
+        Component: () => <BoardCard title="Lorem ipsum dolor sit amet" />,
+      },
+      {
+        id: 3,
+        Component: () => <BoardCard title="Lorem ipsum dolor sit amet" />,
+      },
+      {
+        id: 4,
+        Component: () => <BoardCard title="Lorem ipsum dolor sit amet" />,
+      },
+      {
+        id: 5,
+        Component: () => <BoardCard title="Lorem ipsum dolor sit amet" />,
+      },
+      {
+        id: 6,
+        Component: () => <BoardCard title="Lorem ipsum dolor sit amet" />,
+      },
+      {
+        id: 7,
+        Component: () => <BoardCard title="Lorem ipsum dolor sit amet" />,
+      },
+    ],
+    []
+  );
+
+  return (
+    <GridWrapper
+      items={boardsList}
+      gridItemWidths={{
+        [WIDTHS.xs]: 12,
+        [WIDTHS.sm]: 6,
+        [WIDTHS.md]: 4,
+        [WIDTHS.lg]: 4,
+        [WIDTHS.xl]: 4,
+      }}
+    />
+  );
 };
 
 export default Boards;
